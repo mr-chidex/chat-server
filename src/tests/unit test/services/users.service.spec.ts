88e30e4f79ffb83ef4dbdbc67accba2eba2ddd45
mr-chidex <mbahchiboy2@gmail.com>
2023-03-06 18:42:22 +0100

    update on user service test

--- src/tests/unit test/services/users.service.spec.ts
+++ src/tests/unit test/services/users.service.spec.ts
@@ -14,6 +14,11 @@ describe('UserService', () => {
   });
 
   describe('getProfile', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
     it('should return user details', async () => {
       User.findOne = jest.fn().mockResolvedValue(mockUser);
 
@@ -21,6 +26,47 @@ describe('UserService', () => {
 
       expect(response.success).toBe(true);
       expect(response.data).toEqual(mockUser);
+    });
+  });
+
+  describe('getUsers', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('should return all users', async () => {
+      User.find = jest.fn().mockResolvedValue([mockUser]);
+
+      const response = await userService.getUsers(mockUser);
+
+      expect(response.success).toBe(true);
+      expect(response.data).toEqual([mockUser]);
+    });
+  });
+
+  describe('logout', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('should throw error on invalid user', async () => {
+      User.findOneBy = jest.fn().mockResolvedValue(null);
+
+      expect(userService.logout(mockUser)).rejects.toThrow();
+    });
+
+    it('should logout user', async () => {
+      const save = jest.fn();
+      User.findOneBy = jest.fn().mockResolvedValue({ ...mockUser, save });
+
+      expect(save).not.toHaveBeenCalled();
+
+      const response = await userService.logout(mockUser);
+
+      expect(response.message).toBe('logout successful');
+      expect(save).toHaveBeenCalled();
     });
   });
 });
